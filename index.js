b864f869f1c1895fa34bb1aa8f4376bc2e9be799
deyae loukili <deyae.loukili@ecole-it.com>
2021-02-25 15:48:13 +0100

    app.js

---
+++ index.js
@@ -0,0 +1,17 @@
+let http = require('http')
+let fs = require('fs')
+
+let server = http.createServer()
+server.on('request', (request, response) => {
+    fs.readFile('index.html', (err, data) => {
+        if (err) throw err
+        response.writeHead(200, {
+            'content-type':'text/html; charest=utf-8'
+
+    })
+    response.end(data)
+    })
+    
+})
+
+ server.listen(8080)
